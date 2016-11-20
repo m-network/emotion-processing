1b1ae20967ebdaeb3a10f151246563ee040b849a
Hugh Sato <hughsato@gmail.com>
2016-11-20 17:00:01 -0600

    progress on data to csv, problems with hashtag or non-conforming chars processing through indico

---
+++ apikey.py
@@ -0,0 +1,3 @@
+import indicoio
+indicoio.config.api_key = 'dc352baae6d5672075a17849712e3bec'
+#
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import csv
 from tweetProfile import processedTweet
 import indicoio
-import api-key.py
+import apikey
 # api-key contains: indicoio.config.api_key = '[key]'
 
-with open('xcn.csv') as f:
+m = csv.writer(open('test.csv','wb+'))
+
+with open('testdata.csv') as f:
 	me = csv.reader(f)
 	for i in me:
 		emotionArray = indicoio.emotion(i)
@@ -13,9 +15,13 @@ with open('xcn.csv') as f:
 		mydata = processedTweet(emotionArray[0],i[0])
 
 		# write mydata as a line into CSV file
-		# OR! push it to an array, and run a cleanup task at the end
-		# of the loop to write to file all at once.
-		# ...well...the challenge is holding it all in memory
+		m.writerow(mydata())
+		# m.writerow([i, "joy":Emotions["joy"],
+		# 	"anger":Emotions["anger"],
+		# 	"fear":Emotions["fear"],
+		# 	"sadness":Emotions["sadness"],
+		# 	"surprise":Emotions["surprise"]
+		# )
 
 		print(mydata())
 		print("\r\n")
@@ -23,7 +29,5 @@ with open('xcn.csv') as f:
 		print("done")
 		# python obj to csv using X tool
 		# write into a new CSV
-
-
 
 # ["8cd9ccbc2a7f864edff7306faae88064","sentiment sentencesentiment sentencesentiment sentencesentiment sentence",{"Sentiment":{"joy":.0.1}}],
