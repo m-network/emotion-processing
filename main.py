9e030b527c2d93bddb511bb09d08aca7fcd3c225
Hugh Sato <hughsato@gmail.com>
2016-11-20 15:41:36 -0600

    api key separation file

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import csv
 from tweetProfile import processedTweet
 import indicoio
-indicoio.config.api_key = '[key]'
+import api-key.py
+# api-key contains: indicoio.config.api_key = '[key]'
 
 with open('xcn.csv') as f:
 	me = csv.reader(f)
@@ -10,12 +11,12 @@ with open('xcn.csv') as f:
 		# print(i[0])
 		# print(emotionArray[0])
 		mydata = processedTweet(emotionArray[0],i[0])
-		
+
 		# write mydata as a line into CSV file
 		# OR! push it to an array, and run a cleanup task at the end
 		# of the loop to write to file all at once.
-		# ...well...the challenge is holding it all in memory 
-		
+		# ...well...the challenge is holding it all in memory
+
 		print(mydata())
 		print("\r\n")
 	else:
@@ -26,6 +27,3 @@ with open('xcn.csv') as f:
 
 
 # ["8cd9ccbc2a7f864edff7306faae88064","sentiment sentencesentiment sentencesentiment sentencesentiment sentence",{"Sentiment":{"joy":.0.1}}],
-
-
-	
